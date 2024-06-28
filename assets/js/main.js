608252a7a9132c6790e3b46150739ab3f9d6656a
Sarvesh-Kedia <sarvesh512kedia@gmail.com>
2024-06-28 17:29:15 +0530

    finsihed layout and most content

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,11 +28,31 @@ function move(e){
 
 /*===== GSAP ANIMATION =====*/
 // NAV
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 0.5, delay: 0, y: 10})
+gsap.from('.nav__item', {opacity: 0, duration: 0.5, delay: 0.3, y: 30, stagger: 0.2,})
 
 // HOME
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+gsap.from('.home__title', {opacity: 0, duration: 0.5, delay:0.6, y: 30})
+gsap.from('.home__description', {opacity: 0, duration: 0.5, delay: 1, y: 30})
+gsap.from('.home__button', {opacity: 0, duration: 0.5, delay: 1.6, y: 30})
+gsap.from('.home__img', {opacity: 0, duration: 0.5, delay: 1.3, y: 30})
+
+
+
+
+// App redirect
+function redirectToStore() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    var androidPlayStoreUrl = 'https://play.google.com/store/apps/details?id=fashion.blip.Blip';
+    var iOSAppStoreUrl = 'https://apps.apple.com/app/id6476975641';
+
+    if (/android/i.test(userAgent)) {
+        window.location.href = androidPlayStoreUrl;
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        window.location.href = iOSAppStoreUrl;
+    } else {
+        // Fallback for desktop or unsupported devices
+        // alert('Please visit our website on a mobile device to download the app.');
+        window.location.href = androidPlayStoreUrl;
+    }
+}
